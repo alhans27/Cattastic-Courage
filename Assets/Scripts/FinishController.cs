bb481888c062c350c9813463f8b8ca34c084671a
hanif26 <55867304+hanif26@users.noreply.github.com>
2023-11-08 16:11:02 +0700

    Finish controller

---
+++ Assets/Scripts/FinishController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishController : MonoBehaviour
+{
+    // [SerializeField] private AudioSource finishSoundEffect;
+
+    private bool levelCompleted = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && !levelCompleted)
+        {
+            levelCompleted = true;
+            // finishSoundEffect.Play();
+            Invoke("NextLevel", 3f);
+        }
+    }
+
+    private void NextLevel()
+    {
+        int level = SceneManager.GetActiveScene().buildIndex + 1;
+        // GameManager.Instance.SetLevel(level);
+        SceneManager.LoadScene(level);
+        // Debug.Log("Level yang tersimpan : " + GameManager.Instance.GetLevel());
+    }
+}
